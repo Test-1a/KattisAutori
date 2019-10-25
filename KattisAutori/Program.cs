d8001d3c35f48c60c854c3d66acb40c635c0fb42
DZM <d.bjorlingh@gmail.com>
2019-10-25 15:23:42 +0200

    Add project files.

---
+++ KattisAutori/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace KattisAutori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array.ForEach(Console.ReadLine().Split('-'), (word) => Console.Write(word.First()));
+        }
+    }
+}
